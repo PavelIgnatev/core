07c0fa184fcdfa05e6563e132df525b92f37f13d
Pavel <palllkaignatev@gmail.com>
2024-07-31 19:25:12 +0300

    Release

--- src/helpers/sendToFormBot.ts
+++ src/helpers/sendToFormBot.ts
@@ -5,7 +5,7 @@ export const sendToFormBot = async (text: string) => {
   const token = "7340207766:AAGA80GwPsYYdOfd28_yCSDAwiBAg6XrAcM";
   const sendMessageUrl = `https://api.telegram.org/bot${token}/sendMessage`;
 
-  const chatIds = ["483779758", "324820826"];
+  const chatIds = ["483779758", "324820826", "6957002018"];
 
   try {
     await Promise.all(
