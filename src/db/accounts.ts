9f78f14f7742422421124c59e5a0e7d049e14833
Pavel <palllkaignatev@gmail.com>
2024-10-15 23:46:40 +0600

    Release

--- src/db/accounts.ts
+++ src/db/accounts.ts
@@ -11,6 +11,7 @@ export const getAccounts = async () => {
   const accounts = await accountCollection.distinct('accountId', {
     banned: { $ne: true },
     stopped: { $ne: true },
+    setuped: { $ne: true },
   });
 
   return accounts;
@@ -66,7 +67,7 @@ export const stopAccountsByPrefix = async (prefix: string) => {
   const filteredAccountIds = accountIds.filter((accountId: string) =>
     accountId.includes(prefix)
   );
-  console.log(filteredAccountIds)
+  console.log(filteredAccountIds);
 
   if (filteredAccountIds.length > 0) {
     await accountCollection.updateMany(
