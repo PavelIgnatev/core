cba756dde61ea4e92bbae8b6bf78591dcf0bdce0
Pavel <palllkaignatev@gmail.com>
2024-10-28 14:22:11 +0600

    Release

--- src/index.ts
+++ src/index.ts
@@ -177,9 +177,9 @@ const main = async (ID: string) => {
 getAccounts().then(async (accounts) => {
   console.log({ message: 'ITERATION INIT' });
   const startTime = performance.now();
-  // accounts.forEach((accountId: string) => {
-  promises.push(main('112834976-rus-support'));
-  // });
+  accounts.forEach((accountId: string) => {
+    promises.push(main(accountId));
+  });
 
   const interval = setInterval(() => {
     console.log({
