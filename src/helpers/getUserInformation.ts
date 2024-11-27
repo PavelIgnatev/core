0535b0f4b470536697073d906364ccf9d3f83f4c
Pavel <palllkaignatev@gmail.com>
2024-11-27 16:14:31 +0800

    Release

--- src/helpers/getUserInformation.ts
+++ src/helpers/getUserInformation.ts
@@ -60,8 +60,8 @@ Ensure the extracted name is adjusted to its **${language}** version, either by 
       const userInfo = JSON.parse(resultData?.message?.content?.[0]?.text);
       if (!userInfo.name || !userInfo.gender) {
         await sendToNameBot(`
-        DATA: ${content}
-        RESULT: ${JSON.stringify({ name: null, gender: null })}`);
+DATA: ${content}
+RESULT: ${JSON.stringify({ name: null, gender: null })}`);
 
         return { name: null, gender: null };
       }
