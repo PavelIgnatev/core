1b0d5b363fdddc8ae8e9876774a0430c4cd9aa58
Pavel <palllkaignatev@gmail.com>
2024-12-20 12:49:14 +0700

    Release

--- out.js
+++ out.js
@@ -81039,9 +81039,7 @@ var autoSender = async (client, accountId, tgAccountId) => {
       weekday: "short",
       timeZone: "UTC"
     }).format(/* @__PURE__ */ new Date());
-    if (weekday === "Sat" || weekday === "Sun") {
-      return;
-    }
+    return;
   }
   const remainingTime = new Date(accountByID.remainingTime || currentTime);
   if (currentTime >= remainingTime) {
--- src/modules/autoSender.ts
+++ src/modules/autoSender.ts
@@ -48,9 +48,10 @@ export const autoSender = async (
       timeZone: 'UTC',
     }).format(new Date());
 
-    if (weekday === 'Sat' || weekday === 'Sun') {
-      return;
-    }
+    //weekday === 'Sat' || weekday === 'Sun'
+    // if (weekday === 'Sat' || weekday === 'Sun') {
+    return;
+    // }
   }
 
   const remainingTime = new Date(accountByID.remainingTime || currentTime);
