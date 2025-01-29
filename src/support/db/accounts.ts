ad17cee346d7b4ed7eee7f3df04c447b64956337
Pavel <palllkaignatev@gmail.com>
2025-01-29 07:12:09 +0700

    Release

--- src/support/db/accounts.ts
+++ src/support/db/accounts.ts
@@ -12,7 +12,6 @@ export const getAccountsReLogin = async () => {
     banned: { $ne: true },
     parentAccountId: null,
     workedOut: { $ne: true },
-
     prefix: { $nin: ['test', 'support'] },
   });
 
