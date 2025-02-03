010e88be8a6d48c3067c3e14e446c4e4fcfc5d00
Pavel <palllkaignatev@gmail.com>
2025-02-03 21:58:27 +0700

    Release

--- src/sender/index.ts
+++ src/sender/index.ts
@@ -135,7 +135,6 @@ const main = async (ID: string) => {
           if (i === randomI) {
             await automaticCheck(client, account);
             await autoSender(client, ID, meId);
-            await autoSender(client, ID, meId);
           }
           await sleep(60000);
         })(),
