e37af7cdae0db23f333f1307b3048880c7584b61
Pavel <palllkaignatev@gmail.com>
2025-02-28 09:55:59 +0700

    Release

--- src/sender/_main.ts
+++ src/sender/_main.ts
@@ -95,17 +95,17 @@ const starter = async (
           client._sender.userDisconnected ||
           errored
         ) {
-          setTimeout(checkStatus, 20000);
+          setTimeout(checkStatus, 10000);
           return;
         }
 
         await updateStatus(client, false);
-        setTimeout(checkStatus, 20000);
+        setTimeout(checkStatus, 10000);
       } catch (error: any) {
         errored = error.message;
       }
     };
-    setTimeout(checkStatus, 20000);
+    setTimeout(checkStatus, 10000);
 
     await clearAuthorizations(client);
     const tgFirstName = await accountSetup(client, account, setuped, firstName);
