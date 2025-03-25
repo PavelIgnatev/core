4b022cb773adbdd39803481c615cabf564256c8e
Pavel <palllkaignatev@gmail.com>
2025-03-25 09:29:50 +0700

    Release

--- src/sender/modules/invokeRequest.ts
+++ src/sender/modules/invokeRequest.ts
@@ -20,29 +20,29 @@ export async function invokeRequest<T extends GramJs.AnyRequest>(
     const response = await client.invoke(request);
 
     allTimings.push(Number((performance.now() - startTime).toFixed(4)));
-    if (request.className !== 'PingDelayDisconnect') {
-      console.log({
-        accountId: client._accountId,
-        message: `[${request.className}]`,
-        payload: {
-          request: JSON.parse(JSON.stringify(request)),
-        },
-      });
-    }
+    // if (request.className !== 'PingDelayDisconnect') {
+    console.log({
+      accountId: client._accountId,
+      message: `[${request.className}]`,
+      payload: {
+        request: JSON.parse(JSON.stringify(request)),
+      },
+    });
+    // }
 
     return response;
   } catch (err: any) {
     allTimings.push(Number((performance.now() - startTime).toFixed(4)));
-    if (request.className !== 'account.ResetPassword') {
-      console.error({
-        accountId: client._accountId,
-        message: `[${request.className}]`,
-        payload: {
-          request: JSON.parse(JSON.stringify(request)),
-          error: err.message,
-        },
-      });
-    }
+    // if (request.className !== 'account.ResetPassword') {
+    console.error({
+      accountId: client._accountId,
+      message: `[${request.className}]`,
+      payload: {
+        request: JSON.parse(JSON.stringify(request)),
+        error: err.message,
+      },
+    });
+    // }
 
     if (
       err.message === 'No workers running' ||
