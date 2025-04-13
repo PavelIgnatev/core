e1e738eec21db1a70b5b7db8eb7b272b298c84f0
Pavel <palllkaignatev@gmail.com>
2025-04-13 18:39:08 +0300

    Release

--- sender.js
+++ sender.js
@@ -52908,7 +52908,7 @@ var main = async () => {
   console.log({
     message: "\u{1F4A5} ITERATION INIT \u{1F4A5}"
   });
-  const workers = chunks.slice(0, 10).map((chunk, i) => createWorker(i + 1, chunk));
+  const workers = chunks.map((chunk, i) => createWorker(i + 1, chunk));
   const promises = await Promise.all(workers);
   const successPromises = [];
   for (const promise of promises) {
