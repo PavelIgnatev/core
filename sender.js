91b3c31c9cb8ae0b0d6eb3a995d1503358a1bda3
Pavel <palllkaignatev@gmail.com>
2025-04-16 19:16:46 +0300

    Release

--- sender.js
+++ sender.js
@@ -52908,9 +52908,7 @@ var main = async () => {
   console.log({
     message: "\u{1F4A5} ITERATION INIT \u{1F4A5}"
   });
-  const workers = [
-    createWorker(0, ["6579610286-prefix-female-aisender"])
-  ];
+  const workers = chunks.map((chunk, i) => createWorker(i + 1, chunk));
   const promises = await Promise.all(workers);
   const successPromises = [];
   for (const promise of promises) {
