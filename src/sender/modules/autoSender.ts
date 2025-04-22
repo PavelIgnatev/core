651229cbc072fe2cef7ae3feb93d1e17550eff58
Pavel <palllkaignatev@gmail.com>
2025-04-22 16:39:08 +0300

    Release

--- src/sender/modules/autoSender.ts
+++ src/sender/modules/autoSender.ts
@@ -40,7 +40,7 @@ export const autoSender = async (
   }
 
   if (
-    accountId.includes('aisender') || accountId.includes('casino')
+    accountId.includes('aisender') || accountId.includes('casino') || accountId.includes('stomcenter')
       ? false
       : accountId.includes('prefix')
   ) {
