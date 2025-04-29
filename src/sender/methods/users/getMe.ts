2cb18095362584891a50abd21d15b46f5cef7cb2
Pavel <palllkaignatev@gmail.com>
2025-04-29 19:45:16 +0300

    Release

--- src/sender/methods/users/getMe.ts
+++ src/sender/methods/users/getMe.ts
@@ -1,6 +1,5 @@
 import TelegramClient from '../../../gramjs/client/TelegramClient';
 import GramJs from '../../../gramjs/tl/api';
-import { sendToMainBot } from '../../helpers/sendToMainBot';
 import { invokeRequest } from '../../modules/invokeRequest';
 
 export const getMe = async (client: TelegramClient) => {
@@ -10,19 +9,6 @@ export const getMe = async (client: TelegramClient) => {
       id: new GramJs.InputUserSelf(),
     })
   );
-
-  const stars = await invokeRequest(
-    client,
-    new GramJs.payments.GetStarsStatus({
-      peer: new GramJs.InputPeerSelf(),
-    })
-  );
-
-  const balance = Number(stars?.balance.amount || 0);
-  if (balance > 0) {
-    await sendToMainBot(`💸 BALANCE: ${balance}
-ID: ${client._accountId}`);
-  }
 
   if (!me || me.users[0] instanceof GramJs.UserEmpty) {
     throw new Error('GET_ME_ERROR');
