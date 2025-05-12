1757a0dd9e0cda6ae3196519fc39e7a407045d22
Pavel <palllkaignatev@gmail.com>
2025-05-12 08:27:04 +0300

    Release

--- src/sender/db/accounts.ts
+++ src/sender/db/accounts.ts
@@ -27,6 +27,7 @@ export const getAccountCreationDate = async () => {
       },
       { projection: { accountId: 1 } }
     )
+    .limit(500)
     .toArray();
 
   const accountsWithTimestamp = accounts.map((account) => ({
