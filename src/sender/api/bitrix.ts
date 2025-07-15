8230e18fddc18c1d79a15152e3d66a174d380e51
Pavel <palllkaignatev@gmail.com>
2025-07-15 14:09:56 +0300

    Release

--- src/sender/api/bitrix.ts
+++ src/sender/api/bitrix.ts
@@ -22,26 +22,3 @@ FIELDS: ${JSON.stringify(fields)}`
     throw error;
   }
 };
-
-export const updateBitrixLead = async (
-  webhook: string,
-  id: string,
-  fields: Record<string, any>
-) => {
-  try {
-    const response = await axios.post(`${webhook}/crm.lead.update.json`, {
-      id,
-      fields,
-    });
-
-    return response.data;
-  } catch (error) {
-    await sendToMainBot(
-      `** UPDATE_BITRIX_LEAD_ERROR **
-ERROR: ${error}
-WEBHOOK: ${webhook}
-ID: ${id}
-FIELDS: ${JSON.stringify(fields)}`)
-    throw error;
-  }
-};
